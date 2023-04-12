14d684a37962a23d759c92fbdc4a10d8cf62bf21
Yang Di <young@deelol.com>
2023-04-12 17:16:41 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <el-upload
+      :before-upload="upload"
+    >
+    <el-button>上传</el-button>
+    </el-upload>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted } from 'vue';
+
+function upload(file: any) {
+  console.log(file)
+  const blob = new Blob([JSON.stringify(file)], {type: 'text/plain;charset=utf-8'})
+  console.log(blob)
+  return
+}
+</script>
